583b2bb82bd0c11ad87f0769035bc9c1b1dfa01c
xiaoguangli <gavinli.sh@qq.com>
2018-09-11 19:53:03 +0800

    Create wso2ei on aws

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 AWS-ANSIBLE
 ====
+# Terraform
+```
+cd Terraform-build<br/>
+terraform plan --var-file=terraform.tfvars<br/>
+terraform apply --var-file=terraform.tfvars<br/>
+```
 
-Install aws cli
-https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-welcome.html
-
-
-AWS_PROFILE=credentials ansible-playbook -i inventory/ec2.py AWS.yml
-
+# Ansible<br/>
+```
+cd Ansible-build<br/>
+TF_STATE=../Terraform-build/terraform.tfstate ansible-playbook --inventory-file=/usr/local/bin/terraform-inventory WSO2EI.yml<br/>
+```
--- Terraform-build/main.tf
+++ Terraform-build/main.tf
@@ -7,25 +7,23 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region = "us-east-1"
 }
-  
+
+# module "aws_keypair_module" {
+#   source  = "modules/aws_keypair_module"
+#   key_name = "${var.key_name}"
+#   public_key = "${var.public_key}"
+# }
+
 resource "aws_instance" "wso2ei" {
   ami                    = "ami-04681a1dbd79675a5"
   instance_type          = "t2.micro"  
-  key_name               = "Ansible"
+  key_name               = "developer"
   vpc_security_group_ids = ["${aws_security_group.gg_security_group.id}"]
-  
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-  
   tags = {
     Role = "wso2ei"
     Env = "dev"
   }
-}
-  
+}  
 resource "aws_security_group" "gg_security_group" {
   name = "gg_security_group"
   
